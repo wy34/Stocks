0d147a64de5cbc3836d84cd1b15788bd3b7030a3
William Yeung <williamyeung1141998@gmail.com>
2021-07-28 16:58:49 -0400

    Initial commit and folder structure layout

---
+++ Stocks/Controllers/TopStoriesNewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TopStoriesNewsViewController.swift
+//  Stocks
+//
+//  Created by William Yeung on 7/28/21.
+//
+
+import UIKit
+
+class TopStoriesNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Stocks/Controllers/WatchListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by William Yeung on 7/28/21.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
