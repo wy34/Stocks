4e5b3b34884b1349a43427b4ae8a6a2e188b832d
William Yeung <williamyeung1141998@gmail.com>
2021-07-28 18:03:51 -0400

    Setup searchController on WatchListVC

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class WatchListViewController: UIViewController {
-
+    // MARK: - Properties
+    
+    // MARK: - Views
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        configureUI()
+        layoutUI()
+        setupSearchController()
     }
 
-
+    // MARK: - Helpers
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func layoutUI() {
+        
+    }
+    
+    private func setupSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
 }
 
+// MARK: - UISearchResultsUpdating
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              query.trimmingCharacters(in: .whitespaces) != "" else { return }
+        
+        print(query)
+    }
+}
